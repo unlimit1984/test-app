87680be0e8979bffb1b5beeae5a4ea3f35cc4818
Vladimir Vysokomornyi <unlimit_rmc@mail.ru>
2020-04-24 16:06:12 +0200

    [tests] 424 Remove unused import

--- src/app/user/user.component.spec.ts
+++ src/app/user/user.component.spec.ts
@@ -1,7 +1,6 @@
 import {TestBed} from '@angular/core/testing';
 import {UserComponent} from './user.component';
 import {UserService} from './user.service';
-import compile = WebAssembly.compile;
 
 
 describe('Component: User:', () => {
