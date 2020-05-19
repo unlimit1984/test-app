8bf9ed9d5f2c62906b4b3e930cd2151e72a44673
Vladimir Vysokomornyi <unlimit_rmc@mail.ru>
2020-05-19 09:33:39 +0200

    [tests] Add test with EventEmitter

---
+++ src/app/05-event-emitters/vote.component.spec.ts
@@ -0,0 +1,25 @@
+import { VoteComponent } from './vote.component';
+
+describe('VoteComponent', () => {
+  let component: VoteComponent;
+
+
+  beforeEach(() => {
+    component = new VoteComponent();
+  });
+
+  it('should raise voteChanged event when upvoted', () => {
+    // Arrange
+    let totalVotes = null;
+    component.voteChanged.subscribe(tv => totalVotes = tv);
+
+    // Act
+    component.upVote();
+
+    // Assert
+    // expect(totalVotes).not.toBeNull();
+    // expect(component.totalVotes).toBe(totalVotes);
+    expect(totalVotes).toBe(1);
+
+  });
+});
