e5bfa046519a0086507ad95e1605baf77a924d07
Vladimir Vysokomornyi <unlimit_rmc@mail.ru>
2020-05-19 16:34:34 +0200

    [tests] Add confirmation test

--- src/app/06-services/todos.component.spec.ts
+++ src/app/06-services/todos.component.spec.ts
@@ -48,4 +48,23 @@ describe('TodosComponent', () => {
 
     expect(component.message).toBe(error);
   });
+
+  it('should call the server to delete a todo item if the user confirms', () => {
+    spyOn(window, 'confirm').and.returnValue(true);
+    let spy = spyOn(service, 'delete').and.returnValue(EMPTY);
+
+    component.delete(1);
+
+    expect(spy).toHaveBeenCalled();
+    expect(spy).toHaveBeenCalledWith(1);
+  });
+
+  it('should NOT call the server to delete a todo item if the user cancels', () => {
+    spyOn(window, 'confirm').and.returnValue(false);
+    let spy = spyOn(service, 'delete').and.returnValue(EMPTY);
+
+    component.delete(1);
+
+    expect(spy).not.toHaveBeenCalled();
+  });
 });
